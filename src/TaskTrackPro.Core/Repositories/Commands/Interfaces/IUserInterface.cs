00e2e6a6ab036f4c5106f67c0563b4979a084df3
alisdhameliya <alisdhameliya24@gmail.com>
2025-03-13 11:47:13 +0530

    register backend done

---
+++ src/TaskTrackPro.Core/Repositories/Commands/Interfaces/IUserInterface.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskTrackPro.Core.Models;
+
+namespace TaskTrackPro.Repositories.Interfaces
+{
+    public interface IUserInterface
+    {
+        Task<int> Add(t_User userData);
+        Task<t_User> Login(Login user);
+        Task<t_User> GetUser(int userid);
+        Task<int> ChangePassword(t_User userData);
+        Task<int> UpdateProfile(EditProfile userData);
+
+        Task<t_User> GetUserByEmail(string email);
+    }
+}
