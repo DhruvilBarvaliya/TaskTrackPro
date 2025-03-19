d6c01479a437e0ff2a9921fb43c16c10e4eef5da
DhruvilBarvaliya <dhruvilbarvaliya20@gmail.com>
2025-03-20 00:43:14 +0530

    Firsh commit

--- src/TaskTrackPro.API/Controllers/UserApiController.cs
+++ src/TaskTrackPro.API/Controllers/UserApiController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 using RabbitMQ.Client;
 using StackExchange.Redis;
 using TaskTrackPro.Core.Models;
@@ -69,49 +71,7 @@ namespace TaskTrackPro.API.Controllers
             }
         }
 
-        // [HttpPost("login")]
-        // public async Task<IActionResult> Login([FromForm] Login loginData)
-        // {
-        //     if (!ModelState.IsValid)
-        //     {
-        //         var errors = ModelState.Where(e => e.Value.Errors.Count > 0)
-        //             .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Errors.Select(err => err.ErrorMessage).ToArray());
-
-        //         return BadRequest(new { success = false, message = errors });
-        //     }
-
-        //     try
-        //     {
-        //         if (loginData.c_role == "Admin")
-        //         {
-        //             if (loginData.c_email == "ritadehrawala3@gmail.com" && loginData.c_password == "admin@123")
-        //             {
-        //                 return Ok(new { success = true, message = "Login Success", role = loginData.c_role });
-        //             }
-        //             else
-        //             {
-        //                 return BadRequest(new { success = false, message = "Invalid Credentials" });
-        //             }
-        //         }
-        //         var userData = await _user.Login(loginData);
-        //         if (userData.c_uid == 0)
-        //         {
-        //             return Ok(new { success = false, message = "Invalid email or password" });
-        //         }
-
-        //         HttpContext.Session.SetInt32("userid", userData.c_uid);
-        //         HttpContext.Session.SetString("UserQueue", userData.c_email);
-        //         _redis.SetAdd("online_users", userData.c_email);
-
-        //         return Ok(new { success = true, message = "Login successful", UserData = userData });
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         _logger.LogError(ex, "Error occurred during user login.");
-        //         return StatusCode(500, new { success = false, message = "An unexpected error occurred. Please try again later." });
-        //     }
-        // }
-
+       
         [HttpPost]
         [Route("Login")]
         public async Task<IActionResult> Login([FromForm] Login user)
@@ -208,5 +168,63 @@ namespace TaskTrackPro.API.Controllers
             }
         }
 
+        [HttpPut]
+        [Route("ChangePassword")]
+        public async Task<IActionResult> ChangePassword([FromForm] t_ChangePassword model)
+        {
+            // var userId = HttpContext.Session.GetInt32("c_uid");
+            // if (userId == null) return Unauthorized(new { success = false, message = "User not logged in." });
+
+            var user = await _user.GetUser(model.c_uid);
+            if (user == null) return NotFound(new { success = false, message = "User not found." });
+
+            if (model.OldPassword == null && user.c_password == null && !BCrypt.Net.BCrypt.Verify(model.OldPassword, user.c_password))
+            {
+               
+                return BadRequest(new { success = false, message = "Current password is incorrect" });
+            }
+
+            user.c_password = BCrypt.Net.BCrypt.HashPassword(model.NewPassword);
+            var status = await _user.ChangePassword(user);
+
+            if (status > 0)
+                return Ok(new { success = true, message = "Password changed successfully" });
+
+            return BadRequest(new { success = false, message = "Error changing password" });
+        }
+
+        [HttpPut]
+        [Route("UpdateProfile")]
+        public async Task<IActionResult> UpdateProfile([FromForm] t_UserUpdate model)
+        {
+            // var userId = HttpContext.Session.GetInt32("c_uid");
+            // if (userId == null) return Unauthorized(new { success = false, message = "User not logged in." });
+
+            // var user = await _user.GetUser(userId.Value);
+            // if (user == null) return NotFound(new { success = false, message = "User not found." });
+
+            // user.c_uname = model.c_uname;
+            // user.c_email = model.c_email;
+            // user.c_gender = model.c_gender;
+
+            if (model.c_profile != null && model.c_profile.Length > 0)
+            {
+                var fileName = model.c_email + Path.GetExtension(model.c_profile.FileName);
+                var filePath = Path.Combine("../TaskTrackPro.MVC/wwwroot/profile_images", fileName);
+                model.c_profilepicture = fileName;
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await model.c_profile.CopyToAsync(stream);
+                }
+            }
+
+            var status = await _user.UpdateProfile(model);
+
+            if (status > 0)
+                return Ok(new { success = true, message = "Profile updated successfully" });
+
+            return BadRequest(new { success = false, message = "Error updating profile" });
+        }
     }
 }
